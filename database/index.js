94a5c8b97221c5b827868f07b476374b2ab465b1
izelomabarak <joseluisim.lds@gmail.com>
2025-12-07 23:55:29 -0600

    a

--- database/index.js
+++ database/index.js
@@ -6,32 +6,40 @@ require("dotenv").config()
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool
-if (process.env.NODE_ENV == "development") {
+let pool;
+
+if (process.env.NODE_ENV === "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-})
-
+  });
 // Added for troubleshooting queries
 // during development
 module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
-}
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params);
+        console.log("executed query", { text });
+        return res;
+      } catch (error) {
+        console.error("error in query", { text });
+        throw error;
+      }
+    },
+  };
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  module.exports = {
+    async query(text, params) {
+      return pool.query(text, params);
+    },
+  };
 }
